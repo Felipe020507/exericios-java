9dbb2a8cdfd66d013e7414bd4507079cc188176e
Felipe020507 <129906646+Felipe020507@users.noreply.github.com>
2023-04-10 10:33:01 -0300

    Add files via upload

---
+++ heranca/src/principal/alunoPessoa.java
@@ -0,0 +1,14 @@
+package principal;
+
+import entidade.aluno;
+import entidade.pessoa;
+
+
+public class alunoPessoa {
+
+	public static void main(String[] args) {
+		aluno a = new aluno("Amadeu",20,"Portugues");
+		a.mostar();
+	}
+
+}
