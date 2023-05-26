d8f12ff3c4a6f999d1b60ed25ea322ccdfe54323
Felipe020507 <129906646+Felipe020507@users.noreply.github.com>
2023-05-26 10:36:39 -0300

    Add files via upload

---
+++ AtividadeAvaliativa - Com Comentarios/src/entidades/Disciplina.java
@@ -0,0 +1,42 @@
+/*Enum chamado Disciplina no pacote entidades.*/
+
+package entidades;
+
+/*/*O Enum herda todos os atributos
+ * e metodos da classe Pessoa.*/
+
+public enum Disciplina {
+
+	/*
+	 * FPOO("Fundamentos de Programacao Oriebtada a Objetos"),
+	 * LM("Linguagem de Maracacao"), SO("Sistema Operacional"), REDES("Redes");`:
+	 * Declaração dos valores do enum. São quatro disciplinas representadas por
+	 * constantes: FPOO com o valor
+	 * "Fundamentos de Programacao Oriebtada a Objetos", LM com o valor
+	 * "Linguagem de Maracacao", SO com o valor "Sistema Operacional" e REDES com o
+	 * valor "Redes".
+	 */
+
+	FPOO("fpoo"), LM("lm"), SO("Sistema Operacional"), REDES("Redes");
+
+	private String materias;
+
+	/*
+	 *- private Disciplina(String mateira): Construtor do enum Disciplina que recebe
+	 * um parametro para instanciar um objeto Disciplina com o valor fornecido.
+	 */
+
+	private Disciplina(String materia) {
+		this.materias = materia;
+	}
+
+	/*
+	 * - public String getMaterias(): Metodo getter para obter o nome da
+	 * disciplina.
+	 */
+
+	public String getMaterias() {
+		return materias;
+	}
+
+}
