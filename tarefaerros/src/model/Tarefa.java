3554b1b5814096c127b4bd653dab645f2b4f7caf
Felipe020507 <129906646+Felipe020507@users.noreply.github.com>
2023-08-11 08:51:44 -0300

    Add files via upload

---
+++ tarefaerros/src/model/Tarefa.java
@@ -0,0 +1,22 @@
+package model;
+public class Tarefa {
+    private String descricao;
+    private boolean completado;
+
+    public Tarefa(String description) {
+        this.descricao = description;
+        this.completado = false;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isCompleto() {
+        return completado;
+    }
+
+    public void setCompletado(boolean completed) {
+        this.completado = completed;
+    }
+}
